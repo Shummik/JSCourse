f9996e300c3b8b4f2990d54e2a5a2c07a3ee6585
Gleb Lipovchenko <gllip@mail.ru>
2019-08-26 23:02:36 +0800

    add Exponentiation assignment and jshint es7

--- lesson_2 hard/script.js
+++ lesson_2 hard/script.js
@@ -4,16 +4,17 @@ let num = 266219,
     len = sNumber.length,/* задаем размер числа(получившейся строки) */
     mult = 1;
 
-for (var i = 0; i < len; i += 1) {
+for (let i = 0; i < len; i += 1) {
     output.push(+sNumber.charAt(i)); 
     /* Получаем символ, который занимает позицию i, также можно это сделать с помощью квадратных скобок: [i].
      И затем добавляем push его в массив(знак +sNumer для целочисленного отображения элемента массива */
     mult *=sNumber[i]; 
     console.log(output); /* console log можно вынести за цикл(сделано было для удобства отображения) */
 }
-console.log('Произведение чисел равно : ', mult);
+console.log('Произведение чисел = : ', mult);
 // console.log(output);
 // mult=output.reduce((a,b)=>a*b); /* Перемножаем все элементы массива */
 // console.log(mult);
 
-console.log('Math.pow(mult,3): ', Math.pow(mult,3));
+  mult **= 3; //С помощью нового метода возводим в степень
+console.log('Произведение в кубе = ',mult);
