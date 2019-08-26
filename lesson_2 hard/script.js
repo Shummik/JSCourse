b852ad1a335035cd9c326df7dd8a23723edee6c7
Gleb Lipovchenko <gllip@mail.ru>
2019-08-27 04:12:12 +0800

    Better code lesson2hard

--- lesson_2 hard/script.js
+++ lesson_2 hard/script.js
@@ -1,14 +1,13 @@
+'use strict';
 let num = 266219, 
     output = [], /* Делаем массив из начального числа */
-    sNumber = num.toString(),/* переводим число в строку */
-    len = sNumber.length,/* задаем размер числа(получившейся строки) */
     mult = 1;
 
-for (let i = 0; i < len; i += 1) {
-    output.push(+sNumber.charAt(i)); 
+for (let i = 0; i < num.toString().length; i ++) {
+    output.push(+num.toString().charAt(i)); 
     /* Получаем символ, который занимает позицию i, также можно это сделать с помощью квадратных скобок: [i].
      И затем добавляем push его в массив(знак +sNumer для целочисленного отображения элемента массива */
-    mult *=sNumber[i]; 
+    mult *=num.toString()[i]; 
     console.log(output); /* console log можно вынести за цикл(сделано было для удобства отображения) */
 }
 console.log('Произведение чисел = : ', mult);
@@ -23,7 +22,7 @@ let sMult = mult.toString();
 console.log('Первые 2 цифры полученного числа: ', sMult.slice(0,2));
 // document.write('Первые 2 цифры полученного числа: ', sMult.slice(0,2));
 
-var elem = window.document.createElement("p");
+var elem = document.createElement("p");
       elem.innerHTML = "Первые 2 цифры полученного числа: "+sMult.slice(0,2);
       document.body.appendChild(elem); 
       /* В отладчике выдает ошибку 'ReferenceError: window is not defined' ?(  но все работает*/
