e4418c96fdaf2aa0161c2b78901a45016805dff3
Gleb Lipovchenko <gllip@mail.ru>
2019-08-28 21:39:29 +0800

    Add 3-rd hard lesson

--- lesson_3 hard/script.js
+++ lesson_3 hard/script.js
@@ -1,1 +1,40 @@
+'use strict';
+//  1.
+let lang = 'ru'/* en */;
+let arr;
+// a)
+if (lang == 'ru') {
+   arr = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница'];
+   console.log('Неделя: ', arr);
+}
+else if (lang == 'en') {
+  arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+  console.log('if: ', arr);
+}
+else{
+  console.log('Язык не опознан');
+}
+// b)
+switch(lang){
+  case 'ru':
+    arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+    console.log('switch-case: ', arr);
+    break;
+  case 'en' :
+    arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+    console.log('switch-case: ', arr);
+    break;
+}
+// c)
+let arr2 = {
+	'ru':['пн', 'вт', 'ср','чт', 'пт', 'сб', 'вс'],
+	'en':['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
+};
+console.log('многомерный массив', arr2[lang]);
 
+// 2.
+let namePerson = 'Артем';
+let position  = (namePerson === 'Артем') ? 'Директор' :
+                (namePerson === 'Максим') ? 'Преподаватель':
+                'Студент';
+console.log(position);
