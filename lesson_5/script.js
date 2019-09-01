988351ecdf12abca74fdf1541eb88de9d823eebf
Gleb Lipovchenko <gllip@mail.ru>
2019-09-01 19:37:42 +0800

    Add edits from 4-th lesson

--- lesson_5/script.js
+++ lesson_5/script.js
@@ -2,7 +2,70 @@
 
 // ОБЯЗАТЕЛЬНОЕ ЗАДАНИЕ: 
 // 1) Переписать функцию start циклом do while
-// 2) Добавить валидацию (проверку) на данные которые мы получаем на вопрос 'Во сколько это обойдется?’ в функции  getExpensesMonth
-// 3) Если getTargetMonth возвращает нам отрицательное значение то вместо “Цель будет достигнута”, необходимо выводить “Цель не будет достигнута”
+// 2) Добавить валидацию (проверку) на данные которые мы получаем 
+// на вопрос 'Во сколько это обойдется?’ в функции  getExpensesMonth
+// 3) Если getTargetMonth возвращает нам отрицательное значение 
+// то вместо “Цель будет достигнута”, необходимо выводить “Цель не будет достигнута”
 // 4) Если budgetDay отрицательное значение то вместо уровня дохода пусть выводится сообщение “Что то пошло не так”
 
+let money = +prompt('Ваш ежемесячный доход?');
+let deposit = confirm('Есть ли у Вас депозит в банке?');
+let income = '15';
+
+let question1 = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
+  expenses1 = +prompt('Во сколько это обойдется?'),
+  question2 = prompt('Какие обязательные ежемесячные расходы еще у вас есть?'),
+  expenses2 = +prompt('Во сколько это обойдется?');
+
+let mission = +prompt('Сколько планируете накопить?');
+
+let budgetMonth = money - (expenses1 + expenses2);
+let budgetDay = Math.floor(budgetMonth / 30);
+
+let getExpensesMonth = function () {
+  /*сумма всех расходов за месяц  */
+  return expenses1 + expenses2;
+};
+
+let expenses = getExpensesMonth();
+
+function getAccumulatedMonth() {
+  /*Накопления за месяц  */
+  return money - expenses;
+}
+let accumulatedMonth = getAccumulatedMonth();
+
+function getTargetMonth() {
+  /*За какой период будет достигнута цель  */
+  return Math.ceil(mission / accumulatedMonth);
+}
+let monthPlan = getTargetMonth();
+
+function showTypeof(data) { // выводим тип данных
+  console.log(data, typeof (data));
+}
+
+function getStatusIncome() {
+  if (budgetDay >= 800) {
+    return ("Высокий уровень дохода");
+  } else if (budgetDay >= 300 && budgetDay < 800) {
+    return ("Средний уровень дохода");
+  } else if (budgetDay >= 0 && budgetDay < 300) {
+    return ("Низкий уровень дохода");
+  } else if (budgetDay < 0) {
+    return ("Что то пошло не так");
+  }
+}
+
+
+console.log('expenses: ', expenses);
+console.log('profit: ', accumulatedMonth);
+console.log('monthPlan: ', monthPlan);
+
+console.clear();
+showTypeof(money);
+showTypeof(income);
+showTypeof(deposit);
+console.log(getStatusIncome());
+console.log('Накоплено за период: ' + accumulatedMonth);
+console.log('Цель будет достигнута через: ' + monthPlan + ' месяцев!');
